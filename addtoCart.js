7d242969e12466326e90a105a54286cc6aea10ce
Hadiyah Arif <arifh@uwindsor.ca>
2025-03-30 17:16:26 -0400

    modified:   addtoCart.js new file:   baby.html new file:   baby_boy.html new file:   boys.html modified:   girl.html modified:   homepage.html new file:   myImages/accessory.png new file:   myImages/baby_boy.jpg deleted:    myImages/baby_girl_pants.png deleted:    myImages/babyy.png deleted:    myImages/kid.png deleted:    myImages/kidd.png new file:   myImages/man.jpg deleted:    myImages/men_clothing.jpg deleted:    myImages/nabo.png new file:   myImages/teen_boy.jpg new file:   myImages/toddler_boy.jpg modified:   style.css new file:   teen.html new file:   toddler.html

--- addtoCart.js
+++ addtoCart.js
@@ -1,2 +1,18 @@
 //Add to cart (Handle by JavaScript)
 
+// Array to store cart items
+const cart = [];
+
+// Function to add items to cart
+function addToCart(productName, productPrice) {
+    cart.push({ name: productName, price: productPrice });
+    alert(`${productName} has been added to your cart!`);
+    console.log("Cart:", cart);
+}
+
+// Function to update image and description dynamically based on color selection
+function updateProduct(selectedValue, imgElementId, descElementId) {
+    const [imagePath, description] = selectedValue.split('|');
+    document.getElementById(imgElementId).src = imagePath;
+    document.getElementById(descElementId).textContent = description;
+}
